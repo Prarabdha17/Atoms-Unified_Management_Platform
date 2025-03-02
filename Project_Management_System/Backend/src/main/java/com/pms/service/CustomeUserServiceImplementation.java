8197d7589d4bfce6dc43d4d26718ca72889ce6be
Prarabdha Dabhade <163537407+Prarabdha17@users.noreply.github.com>
2025-03-02 12:12:02 +0530

    Update CustomeUserServiceImplementation.java

--- Project_Management_System/Backend/src/main/java/com/pms/service/CustomeUserServiceImplementation.java
+++ Project_Management_System/Backend/src/main/java/com/pms/service/CustomeUserServiceImplementation.java
@@ -2,14 +2,12 @@ package com.pms.service;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import com.pms.model.User;
 import com.pms.repository.UserRepository;
 
