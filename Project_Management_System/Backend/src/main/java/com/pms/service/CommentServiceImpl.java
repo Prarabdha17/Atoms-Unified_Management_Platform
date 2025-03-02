2b5569b39c1effb34c8a7046b1683aea39d7f94d
Prarabdha Dabhade <163537407+Prarabdha17@users.noreply.github.com>
2025-03-02 12:11:45 +0530

    Update CommentServiceImpl.java

--- Project_Management_System/Backend/src/main/java/com/pms/service/CommentServiceImpl.java
+++ Project_Management_System/Backend/src/main/java/com/pms/service/CommentServiceImpl.java
@@ -10,7 +10,6 @@ import com.pms.repository.IssueRepository;
 import com.pms.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
