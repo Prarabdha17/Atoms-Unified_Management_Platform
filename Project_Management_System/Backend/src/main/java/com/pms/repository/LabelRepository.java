ce5ba7b118d4b3be76dbd0e4381a6319ed5dfacc
Prarabdha Dabhade <163537407+Prarabdha17@users.noreply.github.com>
2025-03-03 23:45:58 +0530

    Update LabelRepository.java

--- Project_Management_System/Backend/src/main/java/com/pms/repository/LabelRepository.java
+++ Project_Management_System/Backend/src/main/java/com/pms/repository/LabelRepository.java
@@ -2,11 +2,8 @@ package com.pms.repository;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.pms.model.User;
-
 import aj.org.objectweb.asm.Label;
 
 //public interface LabelRepository extends JpaRepository<Label, Long> {
