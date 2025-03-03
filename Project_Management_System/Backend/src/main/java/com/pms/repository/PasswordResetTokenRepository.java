c2cdcec21362b51f6830362a0ba76e3b198d9923
Prarabdha Dabhade <163537407+Prarabdha17@users.noreply.github.com>
2025-03-03 23:46:40 +0530

    Update PasswordResetTokenRepository.java

--- Project_Management_System/Backend/src/main/java/com/pms/repository/PasswordResetTokenRepository.java
+++ Project_Management_System/Backend/src/main/java/com/pms/repository/PasswordResetTokenRepository.java
@@ -1,7 +1,6 @@
 package com.pms.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.pms.model.PasswordResetToken;
 
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Integer> {
