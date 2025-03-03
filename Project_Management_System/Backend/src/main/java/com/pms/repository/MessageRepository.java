9a80ceb053036461bb979547bdc35e83db73e106
Prarabdha Dabhade <163537407+Prarabdha17@users.noreply.github.com>
2025-03-03 23:46:25 +0530

    Update MessageRepository.java

--- Project_Management_System/Backend/src/main/java/com/pms/repository/MessageRepository.java
+++ Project_Management_System/Backend/src/main/java/com/pms/repository/MessageRepository.java
@@ -1,9 +1,7 @@
 package com.pms.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.pms.model.Message;
 
 public interface MessageRepository extends JpaRepository<Message, Long>{
