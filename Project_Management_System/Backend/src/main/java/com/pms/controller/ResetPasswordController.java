8ee243f4fa54b18b3f252d421499251ee6856f31
Prarabdha Dabhade <163537407+Prarabdha17@users.noreply.github.com>
2025-03-05 23:18:39 +0530

    Update ResetPasswordController.java

--- Project_Management_System/Backend/src/main/java/com/pms/controller/ResetPasswordController.java
+++ Project_Management_System/Backend/src/main/java/com/pms/controller/ResetPasswordController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.pms.exception.UserException;
 import com.pms.model.PasswordResetToken;
 import com.pms.model.User;
