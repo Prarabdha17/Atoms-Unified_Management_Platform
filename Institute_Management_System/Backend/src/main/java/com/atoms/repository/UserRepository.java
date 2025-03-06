ffe6edb789dbd09482897e2d40ede8fc7ddfde4d
Prarabdha Dabhade <163537407+Prarabdha17@users.noreply.github.com>
2025-03-06 22:30:22 +0530

    Update UserRepository.java

--- Institute_Management_System/Backend/src/main/java/com/atoms/repository/UserRepository.java
+++ Institute_Management_System/Backend/src/main/java/com/atoms/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.atoms.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.atoms.model.User;
 
 public interface UserRepository  extends JpaRepository<User, Long>{
