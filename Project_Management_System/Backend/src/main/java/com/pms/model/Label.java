594f1f95f1ea5c9f3a78c3f0bf3aa126f96b3d47
Prarabdha Dabhade <163537407+Prarabdha17@users.noreply.github.com>
2025-03-08 00:18:51 +0530

    Update Label.java

--- Project_Management_System/Backend/src/main/java/com/pms/model/Label.java
+++ Project_Management_System/Backend/src/main/java/com/pms/model/Label.java
@@ -3,6 +3,7 @@ package com.pms.model;
 import java.time.LocalDateTime;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
